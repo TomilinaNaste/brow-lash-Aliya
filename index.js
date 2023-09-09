7449444f8df48f687f89671a1777abbf1989ec0f
Tomilina Anastacia <anastacia.tomilina@yandex.ru>
2023-09-09 13:43:40 +0300

    сделана прокрутка карусели

---
+++ index.js
@@ -0,0 +1,37 @@
+
+const carouselItems = document.querySelectorAll('.portfolio__images');
+
+carouselItems.forEach((carousel) => {
+  let isDragStart = false;
+  let prevPageX;
+  let prevScrollLeft;
+
+  const dragStart = (e) => {
+     //oбновление значения глобальных переменных при событии нажатия мыши
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+  };
+
+  const dragging = (e) => {
+    //прокрутка изображения влево по указателю мыши
+    if (!isDragStart) return;
+    e.preventDefault();
+    carousel.classList.add('dragging');
+    let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+  };
+
+  const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove('dragging');
+  };
+
+  carousel.addEventListener("mousedown", dragStart);
+  carousel.addEventListener("touchstart", dragStart);
+  carousel.addEventListener("mousemove", dragging);
+  carousel.addEventListener("touchmove", dragging);
+  carousel.addEventListener("mouseup", dragStop);
+  carousel.addEventListener("touchend", dragStop);
+  carousel.addEventListener("touchcancel", dragStop);
+});
